8459544a12f90826108326790c28e2f97abb6e34
Yerlan Akhmetov <kanzhygaly@gmail.com>
2016-09-06 18:29:30 +0600

    Readme, AuthFacadeTest

---
+++ src/main/resources/db/test_data.sql
@@ -0,0 +1,3 @@
+-- Fill user table
+INSERT INTO user (email, password) VALUES ("fpi@bk.ru", "123123");
+INSERT INTO user (email, password) VALUES ("123@gmail.com", "fpifpi");
---
+++ src/test/java/kz/ya/authcore/facade/AuthFacadeTest.java
@@ -0,0 +1,74 @@
+/*
+ * Test AuthFacade Class
+ */
+package kz.ya.authcore.facade;
+
+import javax.ejb.EJB;
+import javax.inject.Inject;
+import javax.security.auth.login.LoginException;
+import kz.ya.authcore.dao.UserDaoLocal;
+import kz.ya.authcore.entity.ApiToken;
+import kz.ya.authcore.entity.User;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import org.junit.Rule;
+import org.junit.rules.ExpectedException;
+
+/**
+ *
+ * @author YERLAN
+ */
+public class AuthFacadeTest {
+    
+    @EJB
+    private UserDaoLocal userDao;
+    @Inject
+    private AuthFacadeLocal authFacade;
+    
+    @Rule
+    public ExpectedException thrown = ExpectedException.none();
+    
+    @Before
+    public void setUp() {
+        User users = new User("fpi@bk.ru", "123123");
+        userDao.save(users);
+        
+        User userf = new User("123@gmail.com", "fpifpi");
+        userDao.save(userf);
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of login method for success result, of class AuthFacade.
+     */
+    @Test
+    public void testLoginSuccess() throws Exception {
+        System.out.println("login success");
+        
+        String username = "fpi@bk.ru";
+        String password = "123123";
+        
+        ApiToken result = authFacade.login(username, password);
+        assertNotNull(result);
+    }
+    
+    /**
+     * Test of login method for fail result, of class AuthFacade.
+     */
+    @Test
+    public void testLoginFail() throws Exception {
+        System.out.println("login fail");
+        
+        String username = "123@gmail.com";
+        String password = "newPassword";
+        
+        thrown.expect(LoginException.class); // test for exception
+        ApiToken result = authFacade.login(username, password);
+        assertNotNull(result);
+    }
+}
